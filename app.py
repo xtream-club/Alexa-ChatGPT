f7e52d89b4592896553e24e137f125e235ebb3aa
xtream_club <107730987+xtream-club@users.noreply.github.com>
2023-08-13 19:24:08 -0600

    Update app.py

--- app.py
+++ app.py
@@ -71,16 +71,11 @@ async def chat_with_gpt(request: Request):
         elif data["request"]["type"] == "IntentRequest":
             # Si es una solicitud de intent, obtener el valor del slot "query"
             query = data["request"]["intent"]["slots"]["query"]["value"]
-            print("es query")
-            print(query)
             response_text = get_chatgpt_response(query)
-            print(response_text)
         else:
             # Si es otro tipo de solicitud, responder con una respuesta aleatoria
             response_text = get_random_response()
-
-        print("response")
-        print(response_text)
+            
         # Mantener la sesión activa para permitir más interacciones
         alexa_response = create_alexa_response(response_text, should_end_session=False)
 
